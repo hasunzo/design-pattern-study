74dad64fb62a2474fe88eb9b7e61b856661774eb
hasunzo <gktjsdl470@gmail.com>
2022-07-23 12:27:36 +0900

    chapter2. observer pattern: 업데이트한 기상 스테이션 코드 테스트

--- src/chapter2/WeatherStation.java
+++ src/chapter2/WeatherStation.java
@@ -1,5 +1,8 @@
 package chapter2;
 
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+
 public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
@@ -8,9 +11,12 @@ public class WeatherStation {
                 new CurrentConditionsDisplay(weatherData);
         HeatIndexDisplay heatIndexDisplay =
                 new HeatIndexDisplay(weatherData);
+        ForecastDisplay forecastDisplay =
+                new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
         weatherData.setMeasurements(78, 90, 29.2f);
+
     }
 }
