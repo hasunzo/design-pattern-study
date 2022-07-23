7ff153116f0100f4389dde09e4544deb101e454b
hasunzo <gktjsdl470@gmail.com>
2022-07-23 11:26:45 +0900

    chapter2. observer pattern: 체감 온도 디스플레이 추가하기

--- src/chapter2/WeatherStation.java
+++ src/chapter2/WeatherStation.java
@@ -6,7 +6,11 @@ public class WeatherStation {
 
         CurrentConditionsDisplay currentConditionsDisplay =
                 new CurrentConditionsDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay =
+                new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
     }
 }
