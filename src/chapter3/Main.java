1e07afaf7433e637cc400e8524f5bb095c83bb99
hasunzo <gktjsdl470@gmail.com>
2022-08-06 20:53:46 +0900

    chapter3. 피자 메인 클래스 만들기

---
+++ src/chapter3/Main.java
@@ -0,0 +1,8 @@
+package chapter3;
+
+public class Main {
+    public static void main(String[] args) {
+        PizzaStore nyPizzaStore = new NYPizzaStore();
+        nyPizzaStore.orderPizza("cheese");
+    }
+}
