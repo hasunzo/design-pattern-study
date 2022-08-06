24d73b9ff31428139ac712a8051c5b06277dbbe9
hasunzo <gktjsdl470@gmail.com>
2022-08-06 20:43:28 +0900

    chapter3. 피자 코드 추가하기

--- src/chapter3/PizzaStore.java
+++ src/chapter3/PizzaStore.java
@@ -9,6 +9,10 @@ public class PizzaStore {
             pizza = new GreekPizza();
         } else if (type.equals("pepperoni")) {
             pizza = new PepperoniPizza();
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new VeggiePizza();
         }
 
         pizza.prepate();
