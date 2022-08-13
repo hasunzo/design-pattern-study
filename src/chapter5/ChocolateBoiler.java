7f37f19c2187eee4c6668258d5d07f57b980f6cd
hasunzo <gktjsdl470@gmail.com>
2022-08-13 20:47:43 +0900

    chapter5. 초콜릿 보일러 코드

---
+++ src/chapter5/ChocolateBoiler.java
@@ -0,0 +1,40 @@
+package chapter5;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (isEmpty() && isBoiled()) {
+            // 끓인 재료를 다음 단계로 넘김
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // 재료를 끓임
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
