6cb5d648d0a218e83cd172a12f66b99d39721e27
hasunzo <gktjsdl470@gmail.com>
2022-09-01 19:41:38 +0900

    chapter6. 차고 커맨드 객체 사용하기

---
+++ src/chapter6/GarageDoor.java
@@ -0,0 +1,23 @@
+package chapter6;
+
+public class GarageDoor {
+    public void up() {
+        System.out.println("차고 문이 열렸습니다.");
+    }
+
+    public void down() {
+        System.out.println("차고 문이 닫혔습니다..");
+    }
+
+    public void stop() {
+        System.out.println("차고 문이 멈췄습니다..");
+    }
+
+    public void lightOn() {
+        System.out.println("차고 문의 불이 켜졌습니다..");
+    }
+
+    public void lightOff() {
+        System.out.println("차고 문의 불이 꺼졌습니다..");
+    }
+}
